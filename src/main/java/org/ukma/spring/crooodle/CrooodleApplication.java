4f49406bb040b8cb0271a62f2544ac282840577e
ZapikDimka <130277188+ZapikDimka@users.noreply.github.com>
2024-09-26 02:50:16 +0300

    coomit

--- src/main/java/org/ukma/spring/crooodle/CrooodleApplication.java
+++ src/main/java/org/ukma/spring/crooodle/CrooodleApplication.java
@@ -1,11 +1,14 @@
 package org.ukma.spring.crooodle;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrooodleApplication {
-
+	@Autowired
+	private ApplicationContext applicationContext;
 	public static void main(String[] args) {
 		SpringApplication.run(CrooodleApplication.class, args);
 	}
