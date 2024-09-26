357ca83a2a9ed614880ac85c26a8f2918112420b
ZapikDimka <130277188+ZapikDimka@users.noreply.github.com>
2024-09-26 12:34:36 +0300

    migrations

--- src/main/java/org/ukma/spring/crooodle/configuration/ApplicationConfiguration.java
+++ src/main/java/org/ukma/spring/crooodle/configuration/ApplicationConfiguration.java
@@ -8,17 +8,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @EnableJpaRepositories("org.ukma.spring.crooodle.repository")
+// Дозволяємо Spring автоматично створювати реалізації репозиторіїв для роботи з базою даних
 @Configuration
+// Позначаємо клас як конфігураційний, він буде обробляти залежності та налаштування
 public class ApplicationConfiguration {
+
     @Value("${app.pbkdf2.secret}")
-    private String pbkdf2Secret;
+    private String pbkdf2Secret; // Значення секрету для алгоритму PBKDF2 із конфігураційного файлу
+
     @Value("${app.pbkdf2.iterations}")
-    private int pbkdf2Iterations;
+    private int pbkdf2Iterations; // Кількість ітерацій для PBKDF2
+
     @Value("${app.pbkdf2.salt.length}")
-    private int pbkdf2SaltLength;
+    private int pbkdf2SaltLength; // Довжина salt для алгоритму PBKDF2
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new Pbkdf2PasswordEncoder(pbkdf2Secret, pbkdf2SaltLength, pbkdf2Iterations, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        // Створюємо та повертаємо об'єкт PasswordEncoder, використовуючи алгоритм PBKDF2 з налаштуваннями
+        return new Pbkdf2PasswordEncoder(pbkdf2Secret, pbkdf2SaltLength, pbkdf2Iterations,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
     }
 }
+
