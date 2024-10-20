ff481608fc37f32dcd3e39fcd95132a88c09ecfe
Andrii Matsevytyi <macevitiya@i.ua>
2024-10-20 16:44:41 +0300

    added jwt tokens (generator and validator classes)

---
+++ src/main/java/org/ukma/spring/crooodle/auth/JwtValidator.java
@@ -0,0 +1,37 @@
+package org.ukma.spring.crooodle.auth;
+
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtValidator extends OncePerRequestFilter {
+
+    private JwtGenerator tokenProvider;
+
+    public JwtValidator(JwtGenerator tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getJwtFromRequest(request);
+        if (token != null && tokenProvider.validateToken(token)) {
+            Long userId = tokenProvider.getUserIdFromJWT(token);
+            // Додати логіку для встановлення аутентифікації користувача
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
+
