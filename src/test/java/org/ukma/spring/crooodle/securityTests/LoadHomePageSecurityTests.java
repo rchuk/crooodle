e5a559f602a21894e3be5ec500d3f800e90801b5
Andrii Matsevytyi <macevitiya@i.ua>
2024-10-28 18:03:16 +0200

    wrote API tests for HotelsController (task2)

--- src/test/java/org/ukma/spring/crooodle/securityTests/LoadHomePageSecurityTests.java
+++ src/test/java/org/ukma/spring/crooodle/securityTests/LoadHomePageSecurityTests.java
@@ -2,12 +2,14 @@ package org.ukma.spring.crooodle.securityTests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
 import org.ukma.spring.crooodle.controller.HomeController;
 import org.ukma.spring.crooodle.service.HotelService;
 
