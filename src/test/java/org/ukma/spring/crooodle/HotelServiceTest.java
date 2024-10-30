f6aeb0a124c7d3a479b7c11fdf124cc8db3bbce8
Volodymyr Slobodian <opitral@proton.me>
2024-10-30 22:42:49 +0200

    add hotel service tests

---
+++ src/test/java/org/ukma/spring/crooodle/HotelServiceTest.java
@@ -0,0 +1,35 @@
+package org.ukma.spring.crooodle;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.ukma.spring.crooodle.dto.HotelResponseDto;
+import org.ukma.spring.crooodle.service.HotelService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.ukma.spring.crooodle.UtilsForTests.*;
+
+@SpringBootTest
+public class HotelServiceTest extends BaseTest {
+
+    @Autowired
+    private HotelService hotelService;
+
+    @Autowired
+    public HotelServiceTest(PasswordEncoder passwordEncoder) {
+        super(passwordEncoder);
+    }
+
+    @Test
+    void testCreateHotel() {
+        Long hotelId = hotelService.createHotel(getRandomHotelCreateRequestDto());
+        HotelResponseDto hotelResponseDto = hotelService.getHotel(hotelId);
+
+        assertNotNull(hotelResponseDto, "Hotel should be created");
+        assertEquals(hotelId, hotelResponseDto.getId(), "Hotel id should be the same");
+    }
+}
