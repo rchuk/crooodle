0f30114f3289bd7d62989b6fdfdd7991c75bdaf4
Volodymyr Slobodian <opitral@proton.me>
2024-10-30 22:47:18 +0200

    remove unused method

--- src/test/java/org/ukma/spring/crooodle/UtilsForTests.java
+++ src/test/java/org/ukma/spring/crooodle/UtilsForTests.java
@@ -3,11 +3,8 @@ package org.ukma.spring.crooodle;
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.ukma.spring.crooodle.dto.BookingDto;
 import org.ukma.spring.crooodle.dto.HotelCreateRequestDto;
 import org.ukma.spring.crooodle.dto.UserRegisterRequestDto;
-
-import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +18,6 @@ public class UtilsForTests {
         userRegisterRequestDto.setEmail(faker.internet().emailAddress());
         userRegisterRequestDto.setPassword(faker.internet().password());
         return userRegisterRequestDto;
-    }
-
-    public static BookingDto getRandomBookingDto(Long roomId) {
-        BookingDto bookingDto = new BookingDto();
-        bookingDto.setRoomId(roomId);
-        bookingDto.setStartDate(LocalDate.now().plusDays(2));
-        bookingDto.setEndDate(LocalDate.now().plusDays(4));
-        return bookingDto;
     }
 
     public static HotelCreateRequestDto getRandomHotelCreateRequestDto() {
