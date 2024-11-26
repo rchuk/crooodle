8b8082abe58d5807610b31858046b3aa0941059f
Andrii Matsevytyi <macevitiya@i.ua>
2024-11-26 13:01:15 +0200

    minor updates in dtos

--- backend/src/main/java/org/ukma/spring/crooodle/dto/HotelCreateRequestDto.java
+++ backend/src/main/java/org/ukma/spring/crooodle/dto/HotelCreateRequestDto.java
@@ -2,8 +2,10 @@ package org.ukma.spring.crooodle.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class HotelCreateRequestDto {
 
