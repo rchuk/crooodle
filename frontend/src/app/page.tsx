fc5f88e1451e8f09d3f8d9353337142d49885f29
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-11-27 11:37:36 +0200

    Added home page placeholder

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,29 @@
-import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react"
+import {Flex, Heading, HStack, Icon, Input, Text, VStack} from "@chakra-ui/react"
+import {Button} from "@/components/ui/button";
+import { RiMapPinLine } from "react-icons/ri";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <VStack height="100vh">
+      <Flex width="100%" flexDirection="column" alignItems="center" rowGap={10} p={5} backgroundColor="bg.subtle">
+        <Flex alignItems="center" columnGap={2}>
+          <Heading size="5xl" color="red.500">
+            Crooodle
+          </Heading>
+          <Heading size="5xl">
+             Find your next stay
+          </Heading>
+          <Icon size="2xl" color="red.500">
+            <RiMapPinLine />
+          </Icon>
+        </Flex>
+        <HStack gap={0} width="100%" maxW="lg">
+          <Input borderTopRightRadius="0" borderBottomRightRadius="0" />
+          <Button borderTopLeftRadius="0" borderBottomLeftRadius="0">
+            Search
+          </Button>
+        </HStack>
+      </Flex>
+    </VStack>
   );
 }
