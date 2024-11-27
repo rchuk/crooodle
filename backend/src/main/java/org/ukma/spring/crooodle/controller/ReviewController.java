37f964b8b41e8ed06d8e9a4e239560ee66d9b4e6
Andrii Matsevytyi <macevitiya@i.ua>
2024-11-27 15:26:51 +0200

    finished Review Controller

---
+++ backend/src/main/java/org/ukma/spring/crooodle/controller/ReviewController.java
@@ -0,0 +1,100 @@
+package org.ukma.spring.crooodle.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import org.ukma.spring.crooodle.dto.*;
+import org.ukma.spring.crooodle.dto.common.PageResponseDto;
+import org.ukma.spring.crooodle.dto.common.PaginationDto;
+import org.ukma.spring.crooodle.service.ReviewService;
+
+
+@RestController
+@RequestMapping("/reviews")
+@SecurityRequirements
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService service;
+
+    @Operation(operationId = "createReview")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PostMapping
+    public ReviewResponseDto createReview(@RequestBody @Valid ReviewCreateRequestDto requestDto) {
+        return service.createReview(requestDto);
+    }
+
+    @Operation(operationId = "updateReview")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PutMapping("/{id}")
+
+    public ReviewResponseDto updateReview(
+        @PathVariable("id") long reviewId,
+        @RequestBody @Valid ReviewEditRequestDto requestDto
+    )
+    {
+        return service.updateReview(reviewId, requestDto);
+    }
+
+    @Operation(operationId = "getReview")
+    @PreAuthorize("permitAll()")
+    @GetMapping("/{id}")
+    public ReviewResponseDto getReview(@PathVariable("id") long reviewId) {
+        return service.getReviewById(reviewId);
+    }
+
+
+    @Operation(operationId = "listReviewsByHotel")
+    @PreAuthorize("permitAll()")
+
+    @GetMapping("/hotel/{hotelId}")
+    public PageResponseDto<ReviewResponseDto> listReviewsByHotel(
+        @PathVariable("hotelId") long hotelId,
+        @ParameterObject @Valid PaginationDto paginationDto
+    )
+    {
+        return service.getReviewsByHotelId(hotelId, paginationDto);
+    }
+
+
+    @Operation(operationId = "listReviewsByUser")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+
+    @GetMapping("/user/{userId}")
+    public PageResponseDto<ReviewResponseDto> listReviewsByUser(
+        @PathVariable("userId") long userId,
+        @ParameterObject @Valid PaginationDto paginationDto
+    )
+    {
+        return service.getReviewsByUserId(userId, paginationDto);
+    }
+
+    @Operation(operationId = "deleteReview")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable("id") long reviewId) {
+        service.deleteReview(reviewId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
