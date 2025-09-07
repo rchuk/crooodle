8f566dfb2ea1dcdbde7a8ac5b261603f7ada5c20
Ruslan Omelchuk <romelchuk12@gmail.com>
2025-09-07 13:16:46 +0300

    Initial commit

---
+++ backend/src/test/java/org/ukma/spring/crooodle/crooodle/CrooodleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ukma.spring.crooodle.crooodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrooodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
