73c5cfc51c68c032d1715b6477fd99ba27f5387d
W1nLin4n <grechkaartema@gmail.com>
2025-10-08 02:06:57 +0300

    Fixed Kubernetes

--- k8s-deploy-dev.bat
+++
@@ -1,7 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl -n ingress-nginx wait --for=condition=Available deploy/ingress-nginx-controller --timeout=180s
-
-docker build --build-arg MODULE_PATH=user-svc/svc -t user-svc:latest -f services/module.Dockerfile .
-docker build --build-arg MODULE_PATH=hotel-svc/svc -t hotel-svc:latest -f services/module.Dockerfile .
-
-kubectl apply -k k8s/overlays/dev
